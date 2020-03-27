8b67caa85135c93f55049170aff5db5c0b610e60
Edouard Lopez <contact@edouard-lopez.com>
2020-03-27 20:12:09 +0100

    setup basic country selector using PatternFly component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import '@patternfly/react-core/dist/styles/base.css';
 import Timeline from './Timeline/Timeline';
+import CountrySelector from './CountrySelector/CountrySelector';
 
 function App() {
   return (
     <div className="App">
+      <CountrySelector/>
       <Timeline />
     </div>
   );
---
+++ src/CountrySelector/CountrySelector.jsx
@@ -0,0 +1,85 @@
+import COUNTRIES from "../data/countries";
+
+import React from "react";
+import { Select, SelectOption, SelectVariant } from "@patternfly/react-core";
+
+class CountrySelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.options = COUNTRIES.map(country => ({
+      value: country,
+      disabled: false
+    }));
+
+    this.state = {
+      isExpanded: false,
+      isPlain: true,
+      selected: []
+    };
+
+    this.onToggle = isExpanded => {
+      this.setState({
+        isExpanded
+      });
+    };
+
+    this.onSelect = (event, selection) => {
+      const { selected } = this.state;
+      if (selected.includes(selection)) {
+        this.setState(
+          prevState => ({
+            selected: prevState.selected.filter(item => item !== selection)
+          }),
+          () => console.log("selections: ", this.state.selected)
+        );
+      } else {
+        this.setState(
+          prevState => ({ selected: [...prevState.selected, selection] }),
+          () => console.log("selections: ", this.state.selected)
+        );
+      }
+    };
+
+    this.clearSelection = () => {
+      this.setState({
+        selected: [],
+        isExpanded: false
+      });
+    };
+  }
+
+  render() {
+    const { isExpanded, isPlain, selected } = this.state;
+    const titleId = "plain-typeahead-select-id";
+
+    return (
+      <div>
+        <span id={titleId} hidden>
+          Select a state
+        </span>
+        <Select
+          variant={SelectVariant.typeaheadMulti}
+          ariaLabelTypeAhead="Select a state"
+          onToggle={this.onToggle}
+          onSelect={this.onSelect}
+          onClear={this.clearSelection}
+          selections={selected}
+          isExpanded={isExpanded}
+          isPlain={isPlain}
+          ariaLabelledBy={titleId}
+          placeholderText="Select a state"
+        >
+          {this.options.map((option, index) => (
+            <SelectOption
+              isDisabled={option.disabled}
+              key={index}
+              value={option.value}
+            />
+          ))}
+        </Select>
+      </div>
+    );
+  }
+}
+
+export default CountrySelector;
