5c464b6c7e7955f4b3ad45963aa2997765797e04
Edouard Lopez <contact@edouard-lopez.com>
2020-04-11 05:23:45 +0200

    update schema

--- src/types/entity.ts
+++ src/types/entity.ts
@@ -1,8 +1,10 @@
-export type Entity =
-  | "government" // Government statement | e.g. Europe, French government, local state or their representative
-  | "health-organization" // e.g. WHO, CDC, ECDC (Europe), CCDC (China), National health organization
-  | "academic" // e.g. University, research lab, etc.
-  | "company" // Company representative | e.g. Dyson, Google, etc.
-  | "individual" // private individual | e.g. Dr. XYZ
-  | "newspaper" // investigation by newspapers BBC, LeMonde, Washington Post
-;
+export const entities = [
+  "government", // Government statement | e.g. Europe, French government, local state or their representative
+  "health-organization", // e.g. WHO, CDC, ECDC (Europe), CCDC (China), National health organization
+  "academic", // e.g. University, research lab, etc.
+  "company", // Company representative | e.g. Dyson, Google, etc.
+  "individual", // private individual | e.g. Dr. XYZ
+  "newspaper", // investigation by newspapers BBC, LeMonde, Washington Post
+] as const;
+
+export type Entity = typeof entities[number];
