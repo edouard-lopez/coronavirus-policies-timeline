a44d7bde44f9550021c4ea2edc00e91bf2a6b4f8
Edouard Lopez <contact@edouard-lopez.com>
2020-04-22 11:37:29 +0200

    add explicit type for Event object

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,14 +1,17 @@
-import { Entity } from "./entity";
-import { Tag } from "./tag";
-import { Region } from "./region";
+import { Entity } from './entity'
+import { Tag } from './tag'
+import { Region } from './region'
+
+type URL = string
+type datetime = string
 
 export interface Event {
-  published_date: string;
-  url: string;
-  title: string;
-  entity: Entity;
-  tags: Array<Tag>;
-  region: Region;
+  published_date: datetime
+  url: URL
+  title: string
+  entity: Entity // who is giving the info, aggregate of source is often put under `newspaper`
+  tags: Array<Tag>
+  region: Region // region described by article
 }
 
-export type Events = Array<Event>;
+export type Events = Array<Event>
