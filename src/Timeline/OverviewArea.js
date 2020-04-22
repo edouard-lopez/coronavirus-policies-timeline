e8d0c830021a667d3633381787809d9e9de8b82c
Edouard Lopez <contact@edouard-lopez.com>
2020-04-22 17:11:32 +0200

    prettify

--- src/Timeline/OverviewArea.js
+++ src/Timeline/OverviewArea.js
@@ -1,6 +1,6 @@
-import { colorize } from "./colors";
-import { addLines } from "./Lines";
-import { addRegionLabels } from "./RegionLabels.js";
+import { colorize } from './colors'
+import { addLines } from './Lines'
+import { addRegionLabels } from './RegionLabels.js'
 
 export const drawOverviewArea = (
   data,
@@ -8,36 +8,36 @@ export const drawOverviewArea = (
   lanes
 ) => {
   const overviewArea = svg
-    .append("g")
+    .append('g')
     .attr(
-      "transform",
-      "translate(" + margin.left + "," + (zoomAreaHeight + margin.top) + ")"
+      'transform',
+      'translate(' + margin.left + ',' + (zoomAreaHeight + margin.top) + ')'
     )
-    .attr("width", width)
-    .attr("height", overviewAreaHeight)
-    .attr("class", "overviewArea");
+    .attr('width', width)
+    .attr('height', overviewAreaHeight)
+    .attr('class', 'overviewArea')
 
   //mini lanes and texts
-  addLines(overviewArea, data, { margin, width, positionAt: yEndPoint });
+  addLines(overviewArea, data, { margin, width, positionAt: yEndPoint })
   addRegionLabels(overviewArea, lanes, {
     margin,
     positionAt: yEndPoint,
-    classes: ["pf-c-chip__text"],
-  });
+    classes: ['pf-c-chip__text'],
+  })
 
   //mini event rects
   overviewArea
-    .append("g")
-    .selectAll(".overview-event")
+    .append('g')
+    .selectAll('.overview-event')
     .data(data)
     .enter()
-    .append("rect")
-    .attr("class", "overview-event")
-    .attr("x", (d) => x(d.start))
-    .attr("y", (d) => yEndPoint(d.lane + 0.5) - 5)
-    .attr("width", (d) => x(d.end - d.start))
-    .attr("height", 10)
-    .attr("style", (d) => colorize(d.lane));
+    .append('rect')
+    .attr('class', 'overview-event')
+    .attr('x', (d) => x(d.start))
+    .attr('y', (d) => yEndPoint(d.lane + 0.5) - 5)
+    .attr('width', (d) => x(d.end - d.start))
+    .attr('height', 10)
+    .attr('style', (d) => colorize(d.lane))
 
-  return overviewArea;
-};
+  return overviewArea
+}
--- src/Timeline/drawGraph.js
+++ src/Timeline/drawGraph.js
@@ -1,48 +1,48 @@
-import * as d3 from "d3";
-import { GRAPH_BEGIN, GRAPH_WIDTH } from "./boundaries";
-import { drawZoomArea } from "./ZoomArea";
-import { drawOverviewArea } from "./OverviewArea";
-import { drawBrush } from "./Brush";
+import * as d3 from 'd3'
+import { GRAPH_BEGIN, GRAPH_WIDTH } from './boundaries'
+import { drawZoomArea } from './ZoomArea'
+import { drawOverviewArea } from './OverviewArea'
+import { drawBrush } from './Brush'
 
 const drawGraph = (element, lanes, data) => {
-  const timeBegin = GRAPH_BEGIN;
-  const timeEnd = GRAPH_WIDTH;
+  const timeBegin = GRAPH_BEGIN
+  const timeEnd = GRAPH_WIDTH
 
-  const graphWidth = element.current.offsetWidth;
+  const graphWidth = element.current.offsetWidth
 
   const margin = {
     top: 20,
     right: 15,
     bottom: 15,
     left: 100,
-  };
-  const width = graphWidth - margin.left - margin.right;
-  const graphHeight = 60 * lanes.length;
-  const height = graphHeight - margin.top - margin.bottom;
+  }
+  const width = graphWidth - margin.left - margin.right
+  const graphHeight = 60 * lanes.length
+  const height = graphHeight - margin.top - margin.bottom
 
-  const zoomAreaHeight = height * 0.8;
-  const overviewAreaHeight = height * 0.2;
+  const zoomAreaHeight = height * 0.8
+  const overviewAreaHeight = height * 0.2
 
-  const x = d3.scaleLinear().domain([timeBegin, timeEnd]).range([0, width]);
+  const x = d3.scaleLinear().domain([timeBegin, timeEnd]).range([0, width])
 
-  const xStartPoint = d3.scaleLinear().range([0, width]);
+  const xStartPoint = d3.scaleLinear().range([0, width])
 
   const yStartPoint = d3
     .scaleLinear()
     .domain([0, lanes.length])
-    .range([0, zoomAreaHeight]);
+    .range([0, zoomAreaHeight])
 
   const yEndPoint = d3
     .scaleLinear()
     .domain([0, lanes.length])
-    .range([0, overviewAreaHeight]);
+    .range([0, overviewAreaHeight])
 
   const svg = d3
     .select(element.current)
-    .append("svg")
-    .attr("class", "chart")
-    .attr("viewBox", [0, 0, graphWidth, graphHeight])
-    .style("display", "block");
+    .append('svg')
+    .attr('class', 'chart')
+    .attr('viewBox', [0, 0, graphWidth, graphHeight])
+    .style('display', 'block')
 
   const chartProps = {
     graphHeight,
@@ -53,24 +53,24 @@ const drawGraph = (element, lanes, data) => {
     overviewAreaHeight,
     svg,
     width,
-  };
+  }
 
   svg
-    .append("defs")
-    .append("clipPath")
-    .attr("id", "clip")
-    .append("rect")
-    .attr("width", width)
-    .attr("height", zoomAreaHeight);
+    .append('defs')
+    .append('clipPath')
+    .attr('id', 'clip')
+    .append('rect')
+    .attr('width', width)
+    .attr('height', zoomAreaHeight)
 
-  const zoomArea = drawZoomArea(data, { ...chartProps, yStartPoint }, lanes);
+  const zoomArea = drawZoomArea(data, { ...chartProps, yStartPoint }, lanes)
   const overviewArea = drawOverviewArea(
     data,
     { ...chartProps, x, yEndPoint },
     lanes
-  );
+  )
   const brushed = () =>
-    drawBrush(data, { x, xStartPoint, yStartPoint }, zoomArea);
+    drawBrush(data, { x, xStartPoint, yStartPoint }, zoomArea)
   //brush
   const brush = d3
     .brushX()
@@ -78,17 +78,17 @@ const drawGraph = (element, lanes, data) => {
       [0, 0],
       [width, overviewAreaHeight],
     ])
-    .on("brush", brushed);
+    .on('brush', brushed)
 
   overviewArea
-    .append("g")
-    .attr("class", "x brush")
+    .append('g')
+    .attr('class', 'x brush')
     .call(brush)
-    .selectAll("rect")
-    .attr("y", 1)
-    .attr("height", overviewAreaHeight - 1);
+    .selectAll('rect')
+    .attr('y', 1)
+    .attr('height', overviewAreaHeight - 1)
 
-  brushed();
-};
+  brushed()
+}
 
-export default drawGraph;
+export default drawGraph
