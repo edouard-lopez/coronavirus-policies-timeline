0accdcfc8292a9125035818fbc727a6b447e9ae1
Edouard Lopez <contact@edouard-lopez.com>
2020-04-22 23:46:33 +0200

    set dynamic pandemy duration min between now or end of 2020

--- src/Timeline/boundaries.test.ts
+++ src/Timeline/boundaries.test.ts
@@ -1,20 +1,43 @@
-import { DateTime } from "luxon";
-import { projectToGraph, FIRST_CASE, MIN_ITEM_WIDTH, GRAPH_BEGIN } from "./boundaries";
+import { DateTime } from 'luxon'
+import {
+  projectToGraph,
+  FIRST_CASE,
+  MIN_ITEM_WIDTH,
+  GRAPH_BEGIN,
+  pandemyDuration,
+  END_OF_2020,
+} from './boundaries'
 
-test("project initial date to graph", () => {
-  const event_start = FIRST_CASE;
-  const event_end = event_start.plus({ days: 1 });
-  const projectedValue = projectToGraph(event_start, event_end);
+test('project initial date to graph', () => {
+  const event_start = FIRST_CASE
+  const event_end = event_start.plus({ days: 1 })
+  const projectedValue = projectToGraph(event_start, event_end)
 
-  expect(projectedValue.start).toBe(GRAPH_BEGIN);
-});
+  expect(projectedValue.start).toBe(GRAPH_BEGIN)
+})
 
-test("projection compute a minimal width", () => {
-  const event_start = DateTime.fromISO("2020-03-17T00:00:00+00:00");
-  const event_end = event_start.plus({ hours: 12 });
+test('projection compute a minimal width', () => {
+  const event_start = DateTime.fromISO('2020-03-17T00:00:00+00:00')
+  const event_end = event_start.plus({ hours: 12 })
 
-  const projectedValue = projectToGraph(event_start, event_end);
+  const projectedValue = projectToGraph(event_start, event_end)
 
-  expect(projectedValue.start).toBe(589);
-  expect(projectedValue.end).toBe(589+MIN_ITEM_WIDTH);
-});
+  expect(projectedValue.start).toBe(589)
+  expect(projectedValue.end).toBe(589 + MIN_ITEM_WIDTH)
+})
+
+test('pandemy duration finish before end of 2020', () => {
+  const lastEvent = DateTime.fromISO('2020-01-31')
+
+  expect(pandemyDuration(lastEvent).as('seconds')).toStrictEqual(
+    lastEvent.diff(FIRST_CASE).as('seconds')
+  )
+})
+
+test('pandemy duration finish at the end of 2020', () => {
+  const lastEvent = DateTime.fromISO('2022-01-31')
+
+  expect(pandemyDuration(lastEvent).as('seconds')).toStrictEqual(
+    END_OF_2020.diff(FIRST_CASE).as('seconds')
+  )
+})
--- src/Timeline/boundaries.ts
+++ src/Timeline/boundaries.ts
@@ -1,8 +1,16 @@
 import { DateTime } from 'luxon'
 
+const pandemyDuration = (now: DateTime = DateTime.fromSeconds(Date.now())) => {
+  const end = DateTime.fromSeconds(
+    Math.min(now.toSeconds(), END_OF_2020.toSeconds())
+  )
+  // const end = now
+  return end.diff(FIRST_CASE)
+}
+
 const FIRST_CASE = DateTime.fromISO('2019-11-17T00:00:00+00:00') // 17 November 2019 https://www.theguardian.com/world/2020/mar/13/first-covid-19-case-happened-in-november-china-government-records-show-report
 const END_OF_2020 = DateTime.fromISO('2020-12-31T23:59:59+00:00')
-const PANDEMY_DURATION = END_OF_2020.diff(FIRST_CASE)
+const PANDEMY_DURATION = pandemyDuration()
 const GRAPH_WIDTH = 2000 // pixels
 const MAP_DATA_TO_GRAPH = PANDEMY_DURATION.as('seconds') / GRAPH_WIDTH
 const MIN_ITEM_WIDTH = 5 // pixels
@@ -28,5 +36,6 @@ export {
   GRAPH_WIDTH,
   MAP_DATA_TO_GRAPH,
   MIN_ITEM_WIDTH,
+  pandemyDuration,
   projectToGraph,
 }
