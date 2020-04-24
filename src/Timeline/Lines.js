722824358c018811de6c58768de575521d7fa5c2
Edouard Lopez <contact@edouard-lopez.com>
2020-04-24 15:23:08 +0200

    add lanes background

--- src/Timeline/Lines.js
+++ src/Timeline/Lines.js
@@ -1,12 +1,27 @@
-export const addLines = (context, data, { margin, width, positionAt }) =>
-  context
+import { getRegionsWithEvents } from '../Region/regions'
+
+export const addLines = (context, data, { margin, width, positionAt }) => {
+  const lanes = context
     .append('g')
-    .selectAll('.event-line')
-    .data(data)
+    .selectAll('.region-lane')
+    .data(getRegionsWithEvents(data))
     .enter()
-    .append('line')
+    .append('g')
+    .attr('class', (d) => `region-lane region-${d}`)
+  lanes
+    .insert('line')
     .attr('x1', margin.right)
-    .attr('y1', (d) => positionAt(d.lane))
+    .attr('y1', (d, i) => positionAt(i))
     .attr('x2', width)
-    .attr('y2', (d) => positionAt(d.lane))
+    .attr('y2', (d, i) => positionAt(i))
     .attr('class', 'event-line')
+  lanes
+    .insert('rect')
+    .attr('x', margin.right)
+    .attr('y', (d, i) => positionAt(i))
+    .attr('width', width)
+    .attr('height', (d, i) => positionAt(i + 1))
+    .attr('class', 'event-lane')
+
+  return lanes
+}
