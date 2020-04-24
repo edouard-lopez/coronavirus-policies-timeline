e138b817a2664fb23160bbc505e1b8a3b01b1f10
Edouard Lopez <contact@edouard-lopez.com>
2020-04-24 14:51:12 +0200

    prettify

--- src/Timeline/RegionLabels.js
+++ src/Timeline/RegionLabels.js
@@ -1,18 +1,18 @@
-const region = (d) => d;
+const region = (d) => d
 
 const addRegionLabels = (context, lanes, { margin, positionAt, classes }) =>
   context
-    .append("g")
-    .selectAll(".region-name")
+    .append('g')
+    .selectAll('.region-name')
     .data(lanes)
     .enter()
-    .append("text")
+    .append('text')
     .text((d) => region(d))
-    .attr("x", -margin.right)
-    .attr("y", (d, i) => positionAt(i + 0.5))
-    .attr("dy", ".5ex")
-    .attr("text-anchor", "end")
-    .attr("x-region", (d, i) => region(d, i))
-    .attr("class", `region-name ${[...classes]}`);
+    .attr('x', -margin.right)
+    .attr('y', (d, i) => positionAt(i + 0.5))
+    .attr('dy', '.5ex')
+    .attr('text-anchor', 'end')
+    .attr('x-region', (d, i) => region(d, i))
+    .attr('class', `region-name ${[...classes]}`)
 
-export { region, addRegionLabels };
+export { region, addRegionLabels }
