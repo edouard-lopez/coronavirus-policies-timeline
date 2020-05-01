d8b0e4f88318e4fb6f1e0afbffa070fd3816069b
Edouard Lopez <contact@edouard-lopez.com>
2020-05-01 23:06:31 +0200

    prettify

--- src/Region/RegionPicker.jsx
+++ src/Region/RegionPicker.jsx
@@ -1,56 +1,56 @@
-import { regionsWithEvents } from "./regions";
+import { regionsWithEvents } from './regions'
 
-import React from "react";
-import { Select, SelectOption, SelectVariant } from "@patternfly/react-core";
+import React from 'react'
+import { Select, SelectOption, SelectVariant } from '@patternfly/react-core'
 
 class RegionPicker extends React.Component {
   constructor(props) {
-    super(props);
-    this.options = regionsWithEvents.map(region => ({
+    super(props)
+    this.options = regionsWithEvents.map((region) => ({
       value: region,
-      disabled: false
-    }));
+      disabled: false,
+    }))
 
     this.state = {
       isExpanded: false,
       isPlain: true,
-      selected: []
-    };
+      selected: [],
+    }
 
-    this.onToggle = isExpanded => {
+    this.onToggle = (isExpanded) => {
       this.setState({
-        isExpanded
-      });
-    };
+        isExpanded,
+      })
+    }
 
     this.onSelect = (event, selection) => {
-      const { selected } = this.state;
+      const { selected } = this.state
       if (selected.includes(selection)) {
         this.setState(
-          prevState => ({
-            selected: prevState.selected.filter(item => item !== selection)
+          (prevState) => ({
+            selected: prevState.selected.filter((item) => item !== selection),
           }),
-          () => console.log("selections: ", this.state.selected)
-        );
+          () => console.log('selections: ', this.state.selected)
+        )
       } else {
         this.setState(
-          prevState => ({ selected: [...prevState.selected, selection] }),
-          () => console.log("selections: ", this.state.selected)
-        );
+          (prevState) => ({ selected: [...prevState.selected, selection] }),
+          () => console.log('selections: ', this.state.selected)
+        )
       }
-    };
+    }
 
     this.clearSelection = () => {
       this.setState({
         selected: [],
-        isExpanded: false
-      });
-    };
+        isExpanded: false,
+      })
+    }
   }
 
   render() {
-    const { isExpanded, isPlain, selected } = this.state;
-    const titleId = "plain-typeahead-select-id";
+    const { isExpanded, isPlain, selected } = this.state
+    const titleId = 'plain-typeahead-select-id'
 
     return (
       <div>
@@ -59,7 +59,7 @@ class RegionPicker extends React.Component {
         </span>
         <Select
           variant={SelectVariant.typeaheadMulti}
-          ariaLabelTypeAhead="Select one or multiple areas"
+          ariaLabelTypeAhead='Select one or multiple areas'
           onToggle={this.onToggle}
           onSelect={this.onSelect}
           onClear={this.clearSelection}
@@ -67,7 +67,7 @@ class RegionPicker extends React.Component {
           isExpanded={isExpanded}
           isPlain={isPlain}
           ariaLabelledBy={titleId}
-          placeholderText="Select one or multiple areas"
+          placeholderText='Select one or multiple areas'
         >
           {this.options.map((option, index) => (
             <SelectOption
@@ -78,8 +78,8 @@ class RegionPicker extends React.Component {
           ))}
         </Select>
       </div>
-    );
+    )
   }
 }
 
-export default RegionPicker;
+export default RegionPicker
